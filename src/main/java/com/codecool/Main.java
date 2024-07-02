1ac1b254435ef4a2f48335a3a5d94584540cdc8a
unknown <szymczyslaw0@gmail.com>
2024-07-02 18:05:22 +0200

    supplier and product CRUD

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -4,14 +4,19 @@ package com.codecool;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ProductDAO productDAO = new ProductDAO();
+        SupplierDAO supplierDAO = new SupplierDAO();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        productDAO.addProduct(1, "Product1", 100, 1, 1);
+
+//        productDAO.updateProduct(1, "UpdatedProduct1", 150, 2, 2);
+
+        productDAO.getProduct(1);
+
+        productDAO.deleteProduct(1);
+
+        supplierDAO.updateSupplier(1,"Company", "firma@gmail.com", 123456789);
+
+        supplierDAO.getSupplier(1);
     }
 }
